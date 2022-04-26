3a11a936b9c48fd59c582eb6e87ea9143d0c5759
vrb9999 <vbiradar9999@gmail.com>
2022-04-26 23:29:07 +0530

    UC-5 Day-3 EmpWage_For_Month

---
+++ emp_wageformonth/emp_wageformonth/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace emp_wageformonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empWageHrs = 0;
+            int empWagePrHrs = 20;
+            int empWorkPrMonth = 20;
+            Random random = new Random();
+            int empAttendence = random.Next(0, 3);
+
+            switch (empAttendence)
+            {
+                case 0:
+                    Console.WriteLine("Employee is not Present");
+                    break;
+                case 1:
+                    Console.WriteLine("Employee is FullTime");
+                    empWageHrs = 8;
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is PartTime");
+                    empWageHrs = 4;
+                    break;
+            }
+            int totalWage = empWorkPrMonth * empWagePrHrs * empWageHrs;
+            Console.WriteLine("Employee total Wage for Month: " + totalWage);
+        }
+    }
+}
