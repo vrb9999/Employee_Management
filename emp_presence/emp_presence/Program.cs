29aa29fa4ba04c0f44edcc4290c3faad60d3d946
vrb9999 <vbiradar9999@gmail.com>
2022-04-26 23:15:23 +0530

    UC-1 Day-3 Emp_Attendance

---
+++ emp_presence/emp_presence/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace emp_presence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int is_full_time = 1;
+            Random obj = new Random();
+            int emp_check = obj.Next(0, 2);
+            if (emp_check == is_full_time)
+            {
+                Console.WriteLine("Emp is present");
+            }
+
+            else
+            {
+                Console.WriteLine("Emp is absent");
+            }
+        }
+    }
+}
